94a13dfe51bd06dc954c7e03020c7ef0ac334c78
Guilherme Lopes <guilherme@gwm-solutions.com>
2018-08-21 17:07:55 +0100

    added xlsx exporter

--- packages/ag-grid-enterprise/src/main.ts
+++ packages/ag-grid-enterprise/src/main.ts
@@ -1,4 +1,43 @@
-import {FiltersToolPanel} from "./sideBar/providedPanels/filters/filtersToolPanel";
+import {ToolPanelAllFiltersComp} from "./sideBar/filter/toolPanelAllFiltersComp";
+
+export {ColumnSelectComp} from "./sideBar/columns/columnsSelect/columnSelectComp";
+export {ToolPanelColumnComp} from "./sideBar/columns/columnsSelect/toolPanelColumnComp";
+export {ColumnGroupComp} from "./sideBar/columns/columnsSelect/columnGroupComp";
+export {AggregationStage} from "./rowStages/aggregationStage";
+export {GroupStage} from "./rowStages/groupStage";
+export {SetFilter} from "./setFilter/setFilter";
+export {SetFilterModel} from "./setFilter/setFilterModel";
+export {StatusPanelComp} from "./statusPanel/statusPanelComp";
+export {StatusPanelService} from "./statusPanel/statusPanelService";
+export {ClipboardService} from "./clipboardService";
+export {EnterpriseBoot} from "./enterpriseBoot";
+export {EnterpriseMenu} from "./menu/enterpriseMenu";
+export {MenuItemComponent} from "./menu/menuItemComponent";
+export {MenuList} from "./menu/menuList";
+export {RangeController} from "./rangeController";
+export {RowGroupColumnsPanel} from "./sideBar/columns/columnDrop/rowGroupColumnsPanel";
+export {ContextMenuFactory} from "./menu/contextMenu";
+export {ViewportRowModel} from "./rowModels/viewport/viewportRowModel";
+export {RichSelectCellEditor} from "./rendering/richSelect/richSelectCellEditor";
+export {RichSelectRow} from "./rendering/richSelect/richSelectRow";
+export {VirtualList} from "./rendering/virtualList";
+export {AbstractColumnDropPanel} from "./sideBar/columns/columnDrop/abstractColumnDropPanel";
+export {PivotColumnsPanel} from "./sideBar/columns/columnDrop/pivotColumnsPanel";
+export {SideBarComp} from "./sideBar/sideBarComp";
+export {LicenseManager} from "./licenseManager";
+export {PivotStage} from "./rowStages/pivotStage";
+export {PivotColDefService} from "./rowStages/pivotColDefService";
+export {PivotModePanel} from "./sideBar/columns/columnDrop/pivotModePanel";
+export {AggFuncService} from "./aggregation/aggFuncService";
+export {MD5} from "./license/md5";
+export {SetFilterListItem} from "./setFilter/setFilterListItem";
+export {ColumnComponent} from "./sideBar/columns/columnDrop/columnComponent";
+export {ValuesColumnPanel} from "./sideBar/columns/columnDrop/valueColumnsPanel";
+export {PivotCompFactory} from "./pivotCompFactory";
+export {RowGroupCompFactory} from "./rowGroupCompFactory";
+export {ExcelCreator} from "./exporter/excelCreator";
+export {ExcelXmlFactory} from "./exporter/excelXmlFactory";
+
 import {Grid} from "ag-grid-community";
 import {EnterpriseMenuFactory} from "./menu/enterpriseMenu";
 import {RangeController} from "./rangeController";
@@ -8,6 +47,7 @@ import {AggregationStage} from "./rowStages/aggregationStage";
 import {EnterpriseBoot} from "./enterpriseBoot";
 import {ContextMenuFactory} from "./menu/contextMenu";
 import {ViewportRowModel} from "./rowModels/viewport/viewportRowModel";
+import {PivotColumnsPanel} from "./sideBar/columns/columnDrop/pivotColumnsPanel";
 import {SideBarComp} from "./sideBar/sideBarComp";
 import {RowGroupCompFactory} from "./rowGroupCompFactory";
 import {LicenseManager} from "./licenseManager";
@@ -19,84 +59,57 @@ import {PivotCompFactory} from "./pivotCompFactory";
 import {MenuItemMapper} from "./menu/menuItemMapper";
 import {ExcelCreator} from "./exporter/excelCreator";
 import {ExcelXmlFactory} from "./exporter/excelXmlFactory";
+import {XlsxFactory} from "./exporter/xlsxFactory";
 import {ServerSideRowModel} from "./rowModels/serverSide/serverSideRowModel";
+import {ColumnSelectHeaderComp} from "./sideBar/columns/columnsSelect/columnSelectHeaderComp";
+import {ColumnContainerComp} from "./sideBar/columns/columnsSelect/columnContainerComp";
 import {HorizontalResizeComp} from "./sideBar/horizontalResizeComp";
-import {ColumnToolPanel} from "./sideBar/providedPanels/columns/columnToolPanel";
-import {SideBarButtonsComp} from "./sideBar/sideBarButtonsComp";
-import {StatusBarService} from "./statusBar/statusBarService";
-import {StatusBar} from "./statusBar/statusBar";
-import {AggregationComp} from "./statusBar/providedPanels/aggregationComp"
-import {NameValueComp} from "./statusBar/providedPanels/nameValueComp";
-import {SelectedRowsComp} from "./statusBar/providedPanels/selectedRowsComp"
-import {TotalRowsComp} from "./statusBar/providedPanels/totalRowsComp"
-import {FilteredRowsComp} from "./statusBar/providedPanels/filteredRowsComp"
-import {TotalAndFilteredRowsComp} from "./statusBar/providedPanels/totalAndFilteredRowsComp"
-import {PrimaryColsHeaderPanel} from "./sideBar/providedPanels/columns/panels/primaryColsPanel/primaryColsHeaderPanel";
-import {PrimaryColsListPanel} from "./sideBar/providedPanels/columns/panels/primaryColsPanel/primaryColsListPanel";
-import {GridHeaderDropZones} from "./sideBar/providedPanels/columns/gridHeaderDropZones";
+import {HeaderColumnDropComp} from "./sideBar/columns/columnDrop/headerColumnDropComp";
+import {ToolPanelColumnComp} from "./sideBar/columns/toolPanelColumnComp";
+import {SideBarSelectComp} from "./sideBar/sideBarSelectComp";
 
-export {AggregationStage} from "./rowStages/aggregationStage";
-export {GroupStage} from "./rowStages/groupStage";
-export {SetFilter} from "./setFilter/setFilter";
-export {SetFilterModel} from "./setFilter/setFilterModel";
-export {StatusBar} from "./statusBar/statusBar";
-export {StatusBarService} from "./statusBar/statusBarService";
-export {ClipboardService} from "./clipboardService";
-export {EnterpriseBoot} from "./enterpriseBoot";
-export {EnterpriseMenu} from "./menu/enterpriseMenu";
-export {MenuItemComponent} from "./menu/menuItemComponent";
-export {MenuList} from "./menu/menuList";
-export {RangeController} from "./rangeController";
-export {RowGroupDropZonePanel} from "./sideBar/providedPanels/columns/panels/rowGroupDropZonePanel";
-export {ContextMenuFactory} from "./menu/contextMenu";
-export {ViewportRowModel} from "./rowModels/viewport/viewportRowModel";
-export {RichSelectCellEditor} from "./rendering/richSelect/richSelectCellEditor";
-export {RichSelectRow} from "./rendering/richSelect/richSelectRow";
-export {VirtualList} from "./rendering/virtualList";
-export {BaseDropZonePanel} from "./sideBar/providedPanels/columns/dropZone/baseDropZonePanel";
-export {PivotDropZonePanel} from "./sideBar/providedPanels/columns/panels/pivotDropZonePanel";
-export {SideBarComp} from "./sideBar/sideBarComp";
-export {LicenseManager} from "./licenseManager";
-export {PivotStage} from "./rowStages/pivotStage";
-export {PivotColDefService} from "./rowStages/pivotColDefService";
-export {PivotModePanel} from "./sideBar/providedPanels/columns/panels/pivotModePanel";
-export {AggFuncService} from "./aggregation/aggFuncService";
-export {MD5} from "./license/md5";
-export {SetFilterListItem} from "./setFilter/setFilterListItem";
-export {DropZoneColumnComp} from "./sideBar/providedPanels/columns/dropZone/dropZoneColumnComp";
-export {ValuesDropZonePanel} from "./sideBar/providedPanels/columns/panels/valueDropZonePanel";
-export {PivotCompFactory} from "./pivotCompFactory";
-export {RowGroupCompFactory} from "./rowGroupCompFactory";
-export {ExcelCreator} from "./exporter/excelCreator";
-export {ExcelXmlFactory} from "./exporter/excelXmlFactory";
+import {StatusPanelService} from "./statusPanel/statusPanelService";
+import {StatusPanelComp} from "./statusPanel/statusPanelComp";
+import {AggregationComponent} from "./statusPanel/aggregationComponent";
+import {StatusPanelValueComponent} from "./statusPanel/statusPanelValueComponent";
+import {SelectedRowCountComponent} from "./statusPanel/selectedRowCountComponent";
+import {TotalRowCountComponent} from "./statusPanel/totalRowCountComponent";
+import {FilteredRowCountComponent} from "./statusPanel/filteredRowCountComponent";
+import {TotalAndFilteredRowCountComponent} from "./statusPanel/totalAndFilteredRowCountComponent";
 
 let rowModelTypes = {viewport: ViewportRowModel, serverSide: ServerSideRowModel};
 
-Grid.setEnterpriseBeans([EnterpriseMenuFactory, ExcelCreator, ExcelXmlFactory, RowGroupCompFactory,
-    PivotCompFactory, RangeController, ClipboardService, PivotStage, PivotColDefService,
-    ContextMenuFactory, GroupStage, AggregationStage, EnterpriseBoot, AggFuncService, LicenseManager, MD5,
-    MenuItemMapper, StatusBarService], rowModelTypes);
+Grid.setEnterpriseBeans([EnterpriseMenuFactory, ExcelCreator, ExcelXmlFactory, XlsxFactory, RowGroupCompFactory, PivotCompFactory,
+    PivotColumnsPanel, RangeController, ClipboardService, PivotStage, PivotColDefService,
+    ContextMenuFactory, GroupStage, AggregationStage, EnterpriseBoot, AggFuncService,
+    LicenseManager, MD5, MenuItemMapper, StatusPanelService], rowModelTypes);
 
 Grid.setEnterpriseComponents([
-    {componentName: 'AgPrimaryColsHeader', theClass: PrimaryColsHeaderPanel},
-    {componentName: 'AgPrimaryColsList', theClass: PrimaryColsListPanel},
+    {componentName: 'AgColumnSelectHeader', theClass: ColumnSelectHeaderComp},
+    {componentName: 'AgColumnContainer', theClass: ColumnContainerComp},
     {componentName: 'AgHorizontalResize', theClass: HorizontalResizeComp},
     {componentName: 'AgSideBar', theClass: SideBarComp},
 
-    {componentName: 'AgStatusBar', theClass: StatusBar},
-    {componentName: 'AgNameValue', theClass: NameValueComp},
+    {componentName: 'AgStatusPanel', theClass: StatusPanelComp},
+    {componentName: 'AgSumAggregationComp', theClass: StatusPanelValueComponent},
+    {componentName: 'AgCountAggregationComp', theClass: StatusPanelValueComponent},
+    {componentName: 'AgMinAggregationComp', theClass: StatusPanelValueComponent},
+    {componentName: 'AgMaxAggregationComp', theClass: StatusPanelValueComponent},
+    {componentName: 'AgAvgAggregationComp', theClass: StatusPanelValueComponent},
+    {componentName: 'AgSelectedRowCountComp', theClass: StatusPanelValueComponent},
 
-    {componentName: 'AgGridHeaderDropZones', theClass: GridHeaderDropZones},
-
-    {componentName: 'AgSideBarButtons', theClass: SideBarButtonsComp},
+    {componentName: 'AgHeaderColumnDrop', theClass: HeaderColumnDropComp},
+    {componentName: 'AgToolPanelColumnComp', theClass: ToolPanelColumnComp},
+    {componentName: 'AgToolPanelAllFiltersComp', theClass: ToolPanelAllFiltersComp},
+    {componentName: 'AgToolPanelSelectComp', theClass: SideBarSelectComp},
 ]);
 
 Grid.setEnterpriseDefaultComponents([
-    {componentName: 'agAggregationComponent', theClass: AggregationComp},
-    {componentName: 'agColumnsToolPanel', theClass: ColumnToolPanel},
-    {componentName: 'agFiltersToolPanel', theClass: FiltersToolPanel},
-    {componentName: 'agSelectedRowCountComponent', theClass: SelectedRowsComp},
-    {componentName: 'agTotalRowCountComponent', theClass: TotalRowsComp},
-    {componentName: 'agFilteredRowCountComponent', theClass: FilteredRowsComp},
-    {componentName: 'agTotalAndFilteredRowCountComponent', theClass: TotalAndFilteredRowsComp}
+    {componentName: 'agAggregationComponent', theClass: AggregationComponent},
+    {componentName: 'agColumnsToolPanel', theClass: ToolPanelColumnComp},
+    {componentName: 'agFiltersToolPanel', theClass: ToolPanelAllFiltersComp},
+    {componentName: 'agSelectedRowCountComponent', theClass: SelectedRowCountComponent},
+    {componentName: 'agTotalRowCountComponent', theClass: TotalRowCountComponent},
+    {componentName: 'agFilteredRowCountComponent', theClass: FilteredRowCountComponent},
+    {componentName: 'agTotalAndFilteredRowCountComponent', theClass: TotalAndFilteredRowCountComponent}
 ]);
